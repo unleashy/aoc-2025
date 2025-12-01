4a8b075782516f2b65f939ca037837bc6a43e186
unleashy <unleashy@users.noreply.github.com>
2025-12-01 18:28:06 -0300

    Day 1 part 1

--- Aoc/Day1.fs
+++ Aoc/Day1.fs
@@ -1,4 +1,19 @@
 ﻿module Aoc.Day1
 
+let private maxDial = 100
+
+let private parseRotation (s: string) =
+  match s with
+  | Etc.Str('L', num) -> maxDial - int num
+  | Etc.Str('R', num) -> int num
+  | s -> failwithf $"invalid rotation {s}"
+
+let private rotate dial n =
+  (dial + n) % maxDial
+
 let part1 input =
-  0
+  input
+  |> Etc.lines
+  |> Seq.map parseRotation
+  |> Seq.scan rotate 50
+  |> Etc.count 0
---
+++ Aoc/Etc.fs
@@ -0,0 +1,15 @@
+﻿module Aoc.Etc
+
+let (|Empty|Str|) (s: string) =
+  if s.Length > 0 then
+    Str(s[0], s[1..])
+  else
+    Empty
+
+let lines (s: string) =
+  s.Split("\n")
+
+let count x xs =
+  xs
+  |> Seq.filter ((=) x)
+  |> Seq.length
